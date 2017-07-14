c93e6c0ef36e1b496a60a646bffb704cd844f973
darwinmorales <darwin.morales.tech@gmail.com>
2017-07-14 19:02:28 +1000

    calculator app

--- calc/index.js
+++ calc/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import ReactPoint from 'react-point'
-import './index.css'
+//import './index.css'
 
 class AutoScalingText extends React.Component {
   state = {
